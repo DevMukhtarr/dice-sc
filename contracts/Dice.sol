4e3d5f2472189ced46e88e602cecd7b4f2285831
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-17 17:32:51 +0100

    created dice smart contract

---
+++ contracts/Dice.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract Dice {
+    address public owner;
+    uint256 public result1;
+    uint256 public result1;
+    
+    constructor() {
+        owner=msg.sender
+    }
+
+    function throwDice() {
+
+    }
+}
