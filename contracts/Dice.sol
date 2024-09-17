24d6ce944b7e982cbb55e66421c4d47f70ca9599
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-17 17:44:56 +0100

    created randomness

--- contracts/Dice.sol
+++ contracts/Dice.sol
@@ -3,14 +3,21 @@ pragma solidity ^0.8.24;
 
 contract Dice {
     address public owner;
-    uint256 public result1;
-    uint256 public result1;
-    
+
     constructor() {
         owner=msg.sender
     }
 
-    function throwDice() {
+    function throwDice() public view returns (uint256, uint256) {
+        require(msg.sender == owner, "Only owner can perform this action");
+        require(msg.sender != address(0), "address 0 detected");
 
+        uint256 randomHash = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender)));
+        uint256 randomHash2 = uint256(keccak256(abi.encodePacked(block.timestamp*3, block.difficulty, msg.sender)));
+
+        uint8 diceResult1 = uint8((randomHash % 6) + 1);
+        uint8 diceResult2 = uint8((randomHash2 % 6) + 1);
+
+        return(diceResult1, diceResult2);
     }
 }
